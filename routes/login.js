b30e47782ed0318d0ba16a0b38b635d8eef4ce36
Your Name <megatron@example.com>
2021-05-04 16:56:25 -0300

    First commit

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 const auth = require("../public/javascripts/auth")
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-  auth.loginUserEmailPass('eudavidreis34.dev@gmail.com','br100300')
+  auth.loginUserEmailPass('YourFirebaseEmail@email.com','YOURPASS123')
   res.send('Logado...');
 });
 
